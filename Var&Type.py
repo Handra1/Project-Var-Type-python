8c057570adc9faae069eee33eff0b4351a36da3d
Han <101443278+Handra1@users.noreply.github.com>
2022-03-14 14:28:39 +0700

    Add files via upload

---
+++ Var&Type.py
@@ -0,0 +1,20 @@
+panjang = 1.52
+lebar = 52.5
+print(panjang)
+print(52.5/1.52 ** 2)
+print(lebar/panjang**2)
+
+hasil = lebar/panjang ** 2
+print(hasil)
+
+print(type(hasil))
+bulan = 30
+print(type(bulan))
+
+x = "contoh pertama"
+y = 'contoh kedua'
+print(type(x))
+print(type(y))
+
+z = True
+print(z)
